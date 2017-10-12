8c0e71a4d2fdd0c8b444b0af0ca8ddbfe42a8c5c
Apoorv Joshi <apoorv@binary.com>
2017-10-12 16:41:21 +0800

    Release to staging

--- js/manifest.js
+++ js/manifest.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"40cf3455fcdcdaa8c1f7","1":"70b0aa43c75d095372e9","2":"6f690e3d9f3033d87abb","3":"c94128f234c5e6bb209f"}[chunkId] + ".min.js";
+/******/ 		script.src = __webpack_require__.p + "" + ({"0":"highstock","1":"vendor","2":"binary.min","3":"webtrader-charts"}[chunkId]||chunkId) + "_" + {"0":"40cf3455fcdcdaa8c1f7","1":"70b0aa43c75d095372e9","2":"80acbb1cb2d0000f4a88","3":"c94128f234c5e6bb209f"}[chunkId] + ".min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
